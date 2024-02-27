10b3d0bae6f7c100101623aae7429d2ec52ba65d
1biancacristina <biancacris_10@hotmail.com>
2024-02-26 22:42:05 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const paragrafo = document.querySelector("p");
+
+paragrafo.addEventListener("click", atualizarNome);
+
+function atualizarNome() {
+    var nome = prompt("Insira um novo nome: ");
+    paragrafo.textContent = "Jogador 1: " + nome;
+}
